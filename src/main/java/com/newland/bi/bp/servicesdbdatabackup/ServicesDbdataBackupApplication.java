cf48899e13055c71b42c1bad00ccf7455be53085
cc <chradium@qq.com>
2019-12-07 16:15:51 +0800

    cc

---
+++ src/main/java/com/newland/bi/bp/servicesdbdatabackup/ServicesDbdataBackupApplication.java
@@ -0,0 +1,8 @@
+package com.newland.bi.bp.servicesdbdatabackup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class ServicesDbdataBackupApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesDbdataBackupApplication.class, args);
+	}
+}
---
+++ src/test/java/com/newland/bi/bp/servicesdbdatabackup/ServicesDbdataBackupApplicationTests.java
@@ -0,0 +1,7 @@
+package com.newland.bi.bp.servicesdbdatabackup;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class ServicesDbdataBackupApplicationTests {
+	@Test void contextLoads() {
+	}
+}
